22c38c8be9fdd1d2a14c3976c94a73de40f9d209
Ryan Rickgauer <rrickgauer1@gmail.com>
2022-11-14 20:18:30 -0600

    add database model

---
+++ src/DbSchemas/DbSchemas.Domain/Enums/DatabaseType.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DbSchemas.Domain.Enums;
+
+public enum DatabaseType : int
+{
+    MySql = 1,
+    SQLite = 2,
+}
---
+++ src/DbSchemas/DbSchemas.Domain/Models/Database.cs
@@ -0,0 +1,21 @@
+﻿using DbSchemas.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DbSchemas.Domain.Models;
+
+public class Database
+{
+    public int? Id { get; set; }
+    public string? Name { get; set; }
+    public DatabaseType? DatabaseType { get; set; }
+    public string? DatabaseName { get; set; }
+    public string? Username { get; set; }
+    public string? Host { get; set; }
+    public string? Password { get; set; }
+    public string? File { get; set; }
+    public DateTime? CreatedOn { get; set; } = DateTime.Now;
+}
