03c3ba0ed063fa942f433f4c1070adc462cee687
Ryan Rickgauer <rrickgauer1@gmail.com>
2022-11-15 09:26:24 -0600

    Branch issue 7 (#8)

---
+++ src/DbSchemas/DbSchemas.Services/DatabaseService.cs
@@ -0,0 +1,100 @@
+﻿using DbSchemas.Domain.Models;
+using DbSchemas.Mappers;
+using DbSchemas.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DbSchemas.Services;
+
+public class DatabaseService
+{
+    private DatabaseRepository _repo;
+    private IModelMapper<Database> _mapper;
+
+    /// <summary>
+    /// Constructor
+    /// </summary>
+    /// <param name="repo"></param>
+    public DatabaseService(DatabaseRepository repo)
+    {
+        _repo = repo;
+        _mapper = new DatabaseMapper();
+    }
+
+    /// <summary>
+    /// Get the database with the matching id
+    /// </summary>
+    /// <param name="id"></param>
+    /// <returns></returns>
+    public async Task<Database?> GetDatabaseAsync(long id)
+    {
+        var databases = await GetDatabasesAsync();
+
+        var result = databases.Where(db => db.Id == id).FirstOrDefault();
+
+        return result;
+    }
+
+
+    /// <summary>
+    /// Get all the databases
+    /// </summary>
+    /// <returns></returns>
+    public async Task<IEnumerable<Database>> GetDatabasesAsync()
+    {
+        // fetch the database table
+        var table = await _repo.SelectAllAsync();
+
+        // map each data row to a Database model object
+        var databases = _mapper.ToModels(table);
+
+        return databases;
+    }
+
+    /// <summary>
+    /// Create the database
+    /// </summary>
+    /// <param name="database"></param>
+    /// <returns></returns>
+    public async Task<bool> InsertDatabaseAsync(Database database)
+    {
+        var numRecords = await _repo.InsertAsync(database);
+
+        var databases = await GetDatabasesAsync();
+        var newDatabase = databases.OrderBy(database => database.Id).Last();
+
+        database.Id = newDatabase.Id;
+
+        return numRecords > 0;
+    }
+
+
+    /// <summary>
+    /// Save the database
+    /// </summary>
+    /// <param name="database"></param>
+    /// <returns></returns>
+    public async Task<bool> SaveDatabaseAsync(Database database)
+    {
+        var numRecords = await _repo.UpdateAsync(database);
+
+        return numRecords > 0;
+    }
+
+    /// <summary>
+    /// Delete the database with the matching id
+    /// </summary>
+    /// <param name="id"></param>
+    /// <returns></returns>
+    public async Task<bool> DeleteDatabaseAsync(long id)
+    {
+        var numRecords = await _repo.DeleteAsync(id);
+
+        return numRecords > 0;
+    }
+
+
+}
--- src/DbSchemas/DbSchemas/Program.cs
+++ src/DbSchemas/DbSchemas/Program.cs
@@ -1,4 +1,7 @@
 ﻿using DbSchemas.Configurations;
+using DbSchemas.Domain.Enums;
+using DbSchemas.Domain.Models;
+using DbSchemas.Repository;
 using DbSchemas.Services;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -6,13 +9,45 @@ using Microsoft.Extensions.DependencyInjection;
 ServiceCollection serviceCollection = new();
 
 serviceCollection.AddScoped<IConfigs, ConfigurationProduction>();
+
 serviceCollection.AddScoped<ProgramDataService>();
+serviceCollection.AddScoped<DatabaseService>();
+
+serviceCollection.AddScoped<DatabaseRepository>();
 
 var serviceProvider = serviceCollection.BuildServiceProvider();
 
 // setup the program data
 var programDataService = serviceProvider.GetRequiredService<ProgramDataService>();
 programDataService.SetupProgramData();
+
+// get the databases
+var databaseService = serviceProvider.GetRequiredService<DatabaseService>();
+var databases = await databaseService.GetDatabasesAsync();
+
+
+
+// insert the database
+//Database newDatabase = new()
+//{
+//    DatabaseName = "database_name",
+//    DatabaseType = DatabaseType.MySql,
+//    Name = "This is the newest one",
+//};
+
+//var insertResult = await databaseService.InsertDatabaseAsync(newDatabase);
+
+
+
+//// udpate database
+//var updateDb = databases.Last();
+//updateDb.Name = "shit fuck cunt ass";
+//await databaseService.SaveDatabaseAsync(updateDb);
+
+
+
+await databaseService.DeleteDatabaseAsync(26);
+
 
 int x = 0;
 
