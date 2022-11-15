bf61975756881ad4d4c597c6767e16d423892985
Ryan Rickgauer <rrickgauer1@gmail.com>
2022-11-14 19:12:24 -0600

    add a new program data service class

---
+++ src/DbSchemas/DbSchemas.Services/ProgramDataService.cs
@@ -0,0 +1,32 @@
+﻿/*****************************************************************************************
+
+This class is responsible for setting up and maintaining the user's sqlite database file.
+
+
+
+*****************************************************************************************/
+
+using DbSchemas.Configurations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DbSchemas.Services;
+
+public class ProgramDataService
+{
+    private readonly IConfigs _configs;
+
+    public ProgramDataService(IConfigs configs)
+    {
+        _configs = configs;
+    }
+
+
+    public void InitProgramDataFiles()
+    {
+
+    }
+}
