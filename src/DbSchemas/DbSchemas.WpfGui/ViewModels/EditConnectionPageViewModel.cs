26c3193794b61b1e5ad579f15540a6337a9dcc8f
Ryan Rickgauer <rrickgauer1@gmail.com>
2022-11-22 15:30:44 -0600

    save the database connection upate

--- src/DbSchemas/DbSchemas.WpfGui/ViewModels/EditConnectionPageViewModel.cs
+++ src/DbSchemas/DbSchemas.WpfGui/ViewModels/EditConnectionPageViewModel.cs
@@ -1,18 +1,24 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 using DbSchemas.Domain.Databases;
 using DbSchemas.Domain.Enums;
 using DbSchemas.Services;
+using DbSchemas.WpfGui.Views.Pages;
 using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using Wpf.Ui.Common.Interfaces;
+using Wpf.Ui.Controls.Interfaces;
+using Wpf.Ui.Mvvm.Contracts;
 
 namespace DbSchemas.WpfGui.ViewModels;
 
 public partial class EditConnectionPageViewModel : ObservableObject, INavigationAware
 {
     private readonly DatabaseConnectionRecordService _connectionRecordService;
+    private readonly INavigation _navigation = App.GetService<INavigationService>().GetNavigationControl();
 
     /// <summary>
     /// Constructor
@@ -28,6 +34,22 @@ public partial class EditConnectionPageViewModel : ObservableObject, INavigation
 
     public IEnumerable<DatabaseType> MyEnumTypeValues => Enum.GetValues(typeof(DatabaseType)).Cast<DatabaseType>();
 
+
+    public bool CanUpdateConnection 
+    { 
+        get
+        {
+            if (string.IsNullOrEmpty(Database.DatabaseConnectionRecord.Name))
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+
+
+
     #region INavigationAware
     public void OnNavigatedFrom()
     {
@@ -39,4 +61,23 @@ public partial class EditConnectionPageViewModel : ObservableObject, INavigation
         //throw new NotImplementedException();
     }
     #endregion
+
+
+    [RelayCommand]
+    public async Task SaveConnectionChangesAsync()
+    {
+        if (!CanUpdateConnection) return;
+
+        await _connectionRecordService.SaveDatabaseAsync(Database.DatabaseConnectionRecord);
+
+        ClosePage();
+    }
+
+
+    public void ClosePage()
+    {
+        _navigation.Navigate(typeof(ConnectionsPage));
+    }
+
+
 }
