c568ddba3bc3868fad1ee4c2afdc88e353189c8f
Ryan Rickgauer <rrickgauer1@gmail.com>
2022-11-28 18:57:02 -0600

    add button to close the page

--- src/DbSchemas/DbSchemas.WpfGui/ViewModels/ViewTablesPageViewModel.cs
+++ src/DbSchemas/DbSchemas.WpfGui/ViewModels/ViewTablesPageViewModel.cs
@@ -1,15 +1,19 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Wpf.Ui.Common.Interfaces;
+using Wpf.Ui.Controls.Interfaces;
+using Wpf.Ui.Mvvm.Contracts;
 
 namespace DbSchemas.WpfGui.ViewModels;
 
 public partial class ViewTablesPageViewModel : ObservableObject, INavigationAware
 {
+    private readonly INavigation _navigation = App.GetService<INavigationService>().GetNavigationControl();
 
     #region - INavigationAware -
     public void OnNavigatedFrom()
@@ -22,4 +26,19 @@ public partial class ViewTablesPageViewModel : ObservableObject, INavigationAwar
         //throw new NotImplementedException();
     }
     #endregion
+
+
+    [ObservableProperty]
+    private bool _isLoading = false;
+
+    /// <summary>
+    /// Close the page and navigate back to the connections page
+    /// </summary>
+    [RelayCommand]
+    public void ClosePage()
+    {
+        _navigation.Navigate(typeof(Views.Pages.ConnectionsPage));
+    }
+
+
 }
