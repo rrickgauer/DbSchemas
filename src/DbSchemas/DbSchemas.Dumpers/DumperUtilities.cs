e444d25325c793a794e93e940d1a45d76561fb9d
Ryan Rickgauer <rrickgauer1@gmail.com>
2022-12-01 08:05:14 -0600

    throw a custom exception if the connection could not be openend

--- src/DbSchemas/DbSchemas.Dumpers/DumperUtilities.cs
+++ src/DbSchemas/DbSchemas.Dumpers/DumperUtilities.cs
@@ -1,4 +1,5 @@
 ﻿using DbSchemas.Domain.ColumnMappers;
+using DbSchemas.Domain.CustomExceptions;
 using DbSchemas.Domain.Models;
 using System;
 using System.Collections.Generic;
@@ -12,19 +13,56 @@ namespace DbSchemas.Domain.Databases;
 
 public static class DumperUtilities
 {
+    /// <summary>
+    /// Execute a select command from the given DbCommand.
+    /// </summary>
+    /// <param name="command"></param>
+    /// <returns></returns>
+    /// <exception cref="InvalidConnectionException">Thrown if the connection could not be opened</exception>
     public static async Task<DataTable> ExecuteQueryAsync(DbCommand command)
     {
-        DataTable dataTable = new();
+        // try to open the connection
+        bool couldConnect = await TryOpenCommandConnectionAsync(command.Connection);
 
-        if (command.Connection.State != ConnectionState.Open)
+        if (!couldConnect)
         {
-            await command.Connection.OpenAsync();
+            throw new InvalidConnectionException();
         }
 
+        // execute the query
         using DbDataReader reader = await command.ExecuteReaderAsync();
+        
+        // load up the datatable with the query results
+        DataTable dataTable = new();
         dataTable.Load(reader);
 
         return dataTable;
+    }
+
+    /// <summary>
+    /// Try to open the given connection
+    /// </summary>
+    /// <param name="connection"></param>
+    /// <returns></returns>
+    private static async Task<bool> TryOpenCommandConnectionAsync(DbConnection connection)
+    {
+        // don't need to open a connection that is already open
+        if (connection.State == ConnectionState.Open)
+        {
+            return true;
+        }
+
+        // try to open the connection
+        try
+        {
+            await connection.OpenAsync();
+        }
+        catch (Exception)
+        {
+            return false;
+        }
+
+        return true;
     }
 
 
