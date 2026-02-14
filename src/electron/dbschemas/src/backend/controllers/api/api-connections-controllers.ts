53979b2e787ba7f15ed8749e22bdd094cf151d97
Ryan Rickgauer <rrickgauer1@gmail.com>
2026-02-13 23:39:10 -0600

    same

--- src/electron/dbschemas/src/backend/controllers/api/api-connections-controllers.ts
+++ src/electron/dbschemas/src/backend/controllers/api/api-connections-controllers.ts
@@ -1,9 +1,9 @@
 
 import { ConnectionModelApiResponseMapper } from "../../../shared/mappers/basic-mappers/connection-mappers";
-import { ControllerArgs } from "../../protocol/router";
+import { ControllerArgs as EndpointArgs } from "../../protocol/router";
 import { diConnectionService } from "../../utilities/dependencies";
 
-export async function getConnections(args: ControllerArgs): Promise<Response>
+export async function getConnections(args: EndpointArgs): Promise<Response>
 {
     const connService = diConnectionService;
     const mapper = new ConnectionModelApiResponseMapper();
@@ -12,8 +12,8 @@ export async function getConnections(args: ControllerArgs): Promise<Response>
     return Response.json(connections);
 }
 
-
-export async function postConnection(): Promise<Response>
+// POST: /api/connections
+export async function postConnection(args: EndpointArgs): Promise<Response>
 {
     return new Response();
 }
